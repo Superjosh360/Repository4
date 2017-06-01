66b94aeb64a65476f6cc456a3570d61637add216
Josh <bionicleboy0@bellsouth.net>
2017-06-01 17:38:56 -0400

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void helloWorld() {
-        System.out.println("Hello World!");
+        System.out.println("Something Different");
     }
 
     public static void main(String[] args) {
