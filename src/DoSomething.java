d3ad30a6c459c87dcde2a840b9a33af557a9d0d8
Superjosh360 <bionicleboy0@bellsouth.net>
2017-06-29 18:46:00 -0400

    added DoSomething and sending it to JoshSub1 branch

---
+++ src/DoSomething.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Josh Rhodes on 6/29/2017.
+ */
+
+public class DoSomething {
+    private int aNumber;
+    private double anotherNumber;
+
+    public DoSomething() {
+        aNumber = 0;
+        anotherNumber = 0;
+    }
+
+    public int getTheNumber() {
+        return aNumber;
+    }
+}
